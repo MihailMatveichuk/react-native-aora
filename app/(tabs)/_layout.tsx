f6553b8d51fb6168dee7e279bc3ed7654e76cd8b
Mikhail Matveichuk <mihkailmatveichuk@gmail.com>
2024-05-19 09:47:12 +0300

    feat: add custom tabs

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,28 +1,64 @@
-import { Tabs, Redirect } from 'expo-router';
+import { Tabs } from 'expo-router';
 
 import { TabIcon } from '../../components';
 import { icons } from '../../constants';
 
+const tabsData = [
+  {
+    name: 'home',
+    title: 'Home',
+    src: 'home',
+  },
+  {
+    name: 'bookmark',
+    title: 'Bookmark',
+    src: 'bookmark',
+  },
+  {
+    name: 'create',
+    title: 'Create',
+    src: 'plus',
+  },
+  {
+    name: 'profile',
+    title: 'Profile',
+    src: 'profile',
+  },
+];
+
 export default function TabsLayout() {
   return (
     <>
-      <Tabs>
-        <Tabs.Screen
-          name="home"
-          options={{
-            title: 'Home',
-            headerShown: false,
-            tabBarIcon: ({ size, color, focused }) => (
-              <TabIcon
-                src={icons.home}
-                color={color}
-                name="home"
-                focused={focused}
-                size={size}
-              />
-            ),
-          }}
-        />
+      <Tabs
+        screenOptions={{
+          tabBarShowLabel: false,
+          tabBarActiveTintColor: '#FFA001',
+          tabBarInactiveTintColor: '#C9C9C9',
+          tabBarStyle: {
+            backgroundColor: '#161622',
+            borderTopWidth: 1,
+            borderTopColor: '#232533',
+            height: 70,
+          },
+        }}
+      >
+        {tabsData.map(({ name, title, src }) => (
+          <Tabs.Screen
+            name={name}
+            options={{
+              title,
+              headerShown: false,
+              tabBarIcon: ({ color, focused }) => (
+                <TabIcon
+                  src={icons[src as keyof typeof icons]}
+                  color={color}
+                  name={title}
+                  focused={focused}
+                />
+              ),
+            }}
+          />
+        ))}
       </Tabs>
     </>
   );
--- components/TabIcon.tsx
+++ components/TabIcon.tsx
@@ -1,18 +1,29 @@
 import React from 'react';
-import { View, Image, type ImageProps } from 'react-native';
+import { View, Image, type ImageProps, Text } from 'react-native';
 
 type Props = {
   src: ImageProps;
   color: string;
   name: string;
   focused: boolean;
-  size: number;
 };
 
-export function TabIcon({ src, color, name, focused, size }: Props) {
+export function TabIcon({ src, color, name, focused }: Props) {
   return (
-    <View>
-      <Image source={src} />
+    <View className="flex flex-col items-center justify-center">
+      <Image
+        source={src}
+        resizeMode="contain"
+        className="w-6 h-6"
+        tintColor={color}
+        alt={name}
+      />
+      <Text
+        className={`${focused ? ' font-psemibold' : ' font-pregular'} text-xs`}
+        style={{ color }}
+      >
+        {name}
+      </Text>
     </View>
   );
 }
